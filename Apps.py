78274166a434488af59be60c8934c6ecb6602bbb
sally306 <sallymailas4@gmail.com>
2025-03-21 16:51:39 +0100

    Update Apps.py

--- Apps.py
+++ Apps.py
@@ -226,4 +226,4 @@ if st.button("Lagebeziehung berechnen"):
         st.write(f"II.  {a2} * t + ({b2}) * s = {c2}")
         
         # Schrittweise Lösung wie im Bild
-        st.write("Nach t auflösen in Gleich"
+        st.write("Nach t auflösen in Gleich")
